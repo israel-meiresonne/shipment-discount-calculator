c9e3550d3e3479bda79d78e78937d4a018b461c5
Israel Meiresonne <israelmeiresonne97@gmail.com>
2024-03-16 19:42:19 +0000

    Date: make first and last day method more flexible

--- src/module/utils/Date.py
+++ src/module/utils/Date.py
@@ -1,5 +1,7 @@
 import datetime
 
+from module.enums import Period
+
 
 class Date:
     """A class to represent a date using a Unix timestamp."""
@@ -13,11 +15,11 @@ class Date:
             date_format (str, optional): The date format. Defaults to
             ISO 8601 format (YYYY-MM-DD).
         """
-        self._datetime = datetime.datetime.strptime(date, format)
+        self._datetime = datetime.datetime.strptime(date, date_format)
 
     def year(self) -> int:
         """
-        Returns the year of the date.
+        To get the year of the date.
 
         Returns:
             int: The year of the date.
@@ -26,7 +28,7 @@ class Date:
 
     def month(self) -> int:
         """
-        Returns the month of the date (1-12).
+        To get the month of the date (1-12).
 
         Returns:
             int: The month of the date.
@@ -35,7 +37,7 @@ class Date:
 
     def day(self) -> int:
         """
-        Returns the day of the date (1-31).
+        To get the day of the date (1-31).
 
         Returns:
             int: The day of the date.
@@ -44,7 +46,7 @@ class Date:
 
     def get_unix_timestamp(self) -> float:
         """
-        Returns the Unix timestamp representing the date.
+        To get the Unix timestamp representing the date.
 
         Returns:
             float: The date as Unix timestamp.
@@ -53,28 +55,72 @@ class Date:
 
     def iso_format(self) -> str:
         """
-        Returns the date in ISO 8601 format (YYYY-MM-DD).
+        To get the date in ISO 8601 format (YYYY-MM-DD).
 
         Returns:
             str: The date in ISO 8601 format.
         """
         return self._datetime.strftime(self.DATE_FORMAT_ISO_8601)
 
-    def first_day_of_month(self) -> int:
+    def first_day_of(self, period: Period) -> float:
         """
-        Returns the Unix timestamp of the first day of the month.
+        To get the first day of the period surrounding the date
+
+        Args:
+            period (Period): Period to get the last day of
 
         Returns:
-            int: The Unix timestamp of the first day of the month.
+            float: The Unix timestamp of last day of the period.
         """
-        return datetime.datetime(self.year(), self.month(), 1).timestamp()
+        match period:
+            case Period.YEAR:
+                params = (self.year(), 1, 1)
+            case Period.MONTH:
+                params = (self.year(), self.month(), 1)
+            case Period.DAY:
+                params = (self.year(), self.month(), self.day())
+            case _:
+                raise ValueError(
+                    f"This Period is not supported: period='{period}'"
+                )
+        return datetime.datetime(*params).timestamp()
 
-    def last_day_of_month(self) -> int:
+    def last_day_of(self, period: Period) -> float:
         """
-        Returns the Unix timestamp of the last day of the month.
+        To get the last day of the period surrounding the date
+
+        Args:
+            period (Period): Period to get the last day of
 
         Returns:
-            int: The Unix timestamp of the last day of the month.
+            float: The Unix timestamp of last day of the period.
         """
-        next_month = datetime.datetime(self.year(), self.month() + 1, 1)
+        match period:
+            case Period.YEAR:
+                params = (self.year() + 1, 1, 1)
+            case Period.MONTH:
+                params = (self.year(), self.month() + 1, 1)
+            case Period.DAY:
+                params = (self.year(), self.month(), self.day() + 1)
+            case _:
+                raise ValueError(
+                    f"This Period is not supported: period='{period}'"
+                )
+        next_month = datetime.datetime(*params)
         return (next_month - datetime.timedelta(days=1)).timestamp()
+
+    def duration_of(self, period: Period) -> float:
+        """
+        To get the duration of the period surrounding the date
+
+        Args:
+            period (Period): Period surrounding the date
+
+        Returns:
+            float: The Unix timestamp of the last day of the month.
+        """
+        first_day_timestamp = self.first_day_of(period)
+        last_day_timestamp = self.last_day_of(period)
+        day_duration = datetime.timedelta(
+            hours=23, minutes=59, seconds=59, microseconds=59)
+        return last_day_timestamp + day_duration - first_day_timestamp
