0fc0251f947dcafc6e684c40c15f11ff86b8959f
Israel Meiresonne <israelmeiresonne97@gmail.com>
2024-03-16 19:39:04 +0000

    transactions: move up attributes `amount`  and `discount` to `TransactionInterface`

--- src/module/transactions/concrete/Transaction.py
+++ src/module/transactions/concrete/Transaction.py
@@ -8,33 +8,9 @@ class Transaction(TransactionInterface):
         super().__init__(date)
         self._provider = provider
         self._size = size
-        self._amount = None
-        self._discount = None
-
-    def set_amount(self, amount: float):
-        self._amount = amount
-
-    def set_discount(self, discount: float):
-        if self._amount:
-            raise Exception(
-                f"The amount must be set first: amount={self._amount}"
-            )
-        if (discount < 0) or (discount > self._amount):
-            detail = f"discount={discount}, amount={self._amount}"
-            raise ValueError(
-                f"The discount must be positive and bellow or equal \
-                    to the amount: {detail}"
-            )
-        self._discount = discount
 
     def get_provider(self) -> Provider:
         return self._provider
 
     def get_size(self) -> Size:
         return self._size
-
-    def get_amount(self) -> None | float:
-        return self._amount
-
-    def get_discount(self) -> None | float:
-        return self._discount
--- src/module/transactions/concrete/TransactionInterface.py
+++ src/module/transactions/concrete/TransactionInterface.py
@@ -1,11 +1,38 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 from module.utils import Date
 
 
 class TransactionInterface(ABC):
+    @abstractmethod
     def __init__(self, date: Date) -> None:
         self._date = date
+        self._amount = None
+        self._discount = None
 
     def get_date(self) -> Date:
         return self._date
+
+    def set_amount(self, amount: float):
+        self._amount = amount
+
+    def set_discount(self, discount: float):
+        if self._amount:
+            raise Exception(
+                f"The amount have to be set first: amount='{self._amount}'"
+            )
+        if (discount <= 0) or (discount > self._amount):
+            detail = f"discount='{discount}', amount='{self._amount}'"
+            raise ValueError(
+                f"The discount have to be '0 < discount <= amount': {detail}"
+            )
+        self._discount = discount
+
+    def get_amount(self) -> None | float:
+        return self._amount
+
+    def get_discount(self) -> None | float:
+        return self._discount
+
+    def __eq__(self, __value: object) -> bool:
+        return self.__dict__ == __value.__dict__
