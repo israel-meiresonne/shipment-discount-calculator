000f4e432a5961999785e99bad86dadecb701aba
Israel Meiresonne <israelmeiresonne97@gmail.com>
2024-03-17 06:11:09 +0000

    main: correctly read, transform and print input

--- src/main.py
+++ src/main.py
@@ -1,15 +1,20 @@
 import sys
 from module import FirstFacade
 
+COUNTRY = 'france'
+FILE_INPUT = 'input.txt'
+
 
 def read_formatteds() -> list[str]:
-    input_filename = sys.argv[1]
-    with open(input_filename, 'r') as f:
-        formatteds = f.read()
-    return formatteds
+    input_filename = sys.argv[1] if len(sys.argv) >= 2 else FILE_INPUT
+    with open(input_filename, 'r') as file:
+        lines = file.readlines()
+    rows = [line.strip() for line in lines]
+    return rows
 
 
 if __name__ == '__main__':
     formatteds = read_formatteds()
-    completed_formatteds = FirstFacade.calculate_shipment(formatteds)
-    print(completed_formatteds)
+    treated_formatteds = FirstFacade.calculate_shipment(COUNTRY, formatteds)
+    for treated_formatted in treated_formatteds:
+        print(treated_formatted)
