63ca6fe13f839184c5f7a913a3ecdf5e1da9209e
Israel Meiresonne <israelmeiresonne97@gmail.com>
2024-03-18 03:15:17 +0000

    ShippingCalculator: check types and generate errors in method `calculate`

--- src/module/calculator/ShippingCalculator.py
+++ src/module/calculator/ShippingCalculator.py
@@ -1,11 +1,12 @@
 from module.core import WrappableCalculator
 from module.transactions import Transaction
-from module.utils import ShippingRates
+from module.utils import Helper, ShippingRates
 
 
 class ShippingCalculator(WrappableCalculator):
     def __init__(self, transaction: Transaction, rates: ShippingRates):
         super().__init__(transaction)
+        Helper.check_type('rates', rates, ShippingRates)
         self._rates = rates
 
     def calculate(self) -> Transaction:
@@ -13,6 +14,11 @@ class ShippingCalculator(WrappableCalculator):
         transaction: Transaction = self._transaction
         providers = [transaction.get_provider()]
         sizes = [transaction.get_size()]
-        record = rates.get(providers, sizes)[0]
-        transaction.set_amount(record.get_price())
+        records = rates.get(providers, sizes)
+        n_record = len(records)
+        if n_record == 0:
+            raise AttributeError(f"No record match: records='{records}'")
+        if n_record > 1:
+            raise AttributeError(f"Too many record match: records='{records}'")
+        transaction.set_amount(records[0].get_price())
         return transaction
