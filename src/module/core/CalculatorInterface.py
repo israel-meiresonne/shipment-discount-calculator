7692d74622fbeddb122a018e3b2701b6cafc7fb8
Israel Meiresonne <israelmeiresonne97@gmail.com>
2024-03-18 03:12:05 +0000

    CalculatorInterface: check types

--- src/module/core/CalculatorInterface.py
+++ src/module/core/CalculatorInterface.py
@@ -1,10 +1,12 @@
 from abc import ABC, abstractmethod
 
 from module.transactions import TransactionInterface
+from module.utils import Helper
 
 
 class CalculatorInterface(ABC):
     def __init__(self, transaction: TransactionInterface) -> None:
+        Helper.check_type('transaction', transaction, TransactionInterface)
         self._transaction = transaction
 
     @abstractmethod
