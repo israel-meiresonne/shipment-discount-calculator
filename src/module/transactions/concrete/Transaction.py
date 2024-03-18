9ab4881e4c8460f641426a1cb98582cb65a97318
Israel Meiresonne <israelmeiresonne97@gmail.com>
2024-03-18 03:06:31 +0000

    Transaction: check types

--- src/module/transactions/concrete/Transaction.py
+++ src/module/transactions/concrete/Transaction.py
@@ -1,11 +1,19 @@
 from module.enums import Provider, Size
-from module.utils import Date
-from . import TransactionInterface
+from module.utils import Date, Helper
+from .TransactionInterface import TransactionInterface
 
 
 class Transaction(TransactionInterface):
-    def __init__(self, date: Date, provider: Provider, size: Size):
-        super().__init__(date)
+    def __init__(
+            self,
+            date: Date,
+            provider: Provider,
+            size: Size,
+            amount: float = None,
+            discount: float = None):
+        super().__init__(date, amount, discount)
+        Helper.check_type('provider', provider, Provider)
+        Helper.check_type('size', size, Size)
         self._provider = provider
         self._size = size
 
