97e7769393fbdae226b9916d3e902093e60b0163
Israel Meiresonne <israelmeiresonne97@gmail.com>
2024-03-18 02:52:22 +0000

    ShippingRates: move recursive method to `Helper`, store reusable data and check constructor's parameters

--- src/module/utils/ShippingRates.py
+++ src/module/utils/ShippingRates.py
@@ -1,57 +1,35 @@
 from module.enums import Provider, Size
 from .ShippingRecord import ShippingRecord
+from .Helper import Helper
 
 
 class ShippingRates:
+    _PROVIDERS = ()
+    _SIZES = ()
+
     def __init__(self, rates: dict) -> None:
+        Helper.check_type('rates', rates, dict)
         self._rates = rates
+        if len(self._PROVIDERS) == 0:
+            self._PROVIDERS = tuple(Provider.__members__.values())
+        if len(self._SIZES) == 0:
+            self._SIZES = tuple(Size.__members__.values())
 
     def get(
             self,
-            providers: list[Provider] = [],
-            sizes: list[Size] = []) -> list[ShippingRecord]:
+            providers: list[Provider] = (),
+            sizes: list[Size] = ()) -> list[ShippingRecord]:
         rates = self._rates
+        if len(providers) == 0:
+            providers = self._PROVIDERS
+        if len(sizes) == 0:
+            sizes = self._SIZES
         records = set()
         for provider in providers:
             for size in sizes:
                 keys = (provider, size)
-                price = self._recursive_get(rates, keys)
-                record = ShippingRecord(*keys, price)
-                records.add(record)
+                price = Helper._recursive_get(rates, keys)
+                if price is not  None:
+                    record = ShippingRecord(*keys, price)
+                    records.add(record)
         return list(records)
-
-    @classmethod
-    def _recursive_get(cls, data, keys):
-        """
-        Recursively retrieves a value from a nested dictionary using
-        a list of keys.
-
-        Args:
-            data: The nested dictionary to search.
-            keys: A list of keys representing the path to the desired value.
-
-        Returns:
-            The value found at the specified path within the nested dictionary,
-            or None if the path doesn't exist.
-        """
-        if not keys:
-            return data
-        key = keys[0]
-        if cls.is_iterable(data) and (key in data):
-            return cls._recursive_get(data[key], keys[1:])
-        else:
-            return None
-
-    @staticmethod
-    def is_iterable(obj) -> bool:
-        """
-        Checks if a value is iterable using isinstance().
-
-        Args:
-            obj: The value to check.
-
-        Returns:
-            True if the value is iterable, False otherwise.
-        """
-        return isinstance(obj, (str, list, tuple, set, dict)) \
-            or hasattr(obj, '__iter__')
